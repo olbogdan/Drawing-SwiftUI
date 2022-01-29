f2c7594aac89f2891efda1544b19b6143c5fa531
Oleksandr Bogdanov <oleksandr.bogdanov@grandcentrix.net>
2022-01-30 00:19:46 +0100

    Initial Commit

---
+++ Drawing SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Drawing SwiftUI
+//
+//  Created by Oleksandr Bogdanov on 30.01.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Drawing SwiftUI/Drawing_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Drawing_SwiftUIApp.swift
+//  Drawing SwiftUI
+//
+//  Created by Oleksandr Bogdanov on 30.01.22.
+//
+
+import SwiftUI
+
+@main
+struct Drawing_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
